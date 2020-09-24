0153a36ec515ce3e8c8878358f2d43dab4286217
cek333 <69444825+cek333@users.noreply.github.com>
2020-09-23 23:48:22 -0400

    added (dummy) questions file

---
+++ assets/js/questions.js
@@ -0,0 +1,92 @@
+let quizQuestions = [
+  { 
+    question: "question-1 question-1 question-1",
+    choice1: "question-1 choice-1",
+    choice2: "question-1 choice-2",
+    choice3: "question-1 choice-3",
+    choice4: "question-1 choice-4",
+    id: 'xxx',
+    answer: 2
+  },
+  { 
+    question: "question-2 question-2 question-2",
+    choice1: "question-2 choice-1",
+    choice2: "question-2 choice-2",
+    choice3: "question-2 choice-3",
+    choice4: "question-2 choice-4",
+    id: 'xxx',
+    answer: 1
+  },
+  { 
+    question: "question-3 question-3 question-3",
+    choice1: "question-3 choice-1",
+    choice2: "question-3 choice-2",
+    choice3: "question-3 choice-3",
+    choice4: "question-3 choice-4",
+    id: 'xxx',
+    answer: 3
+  },
+  { 
+    question: "question-4 question-4 question-4",
+    choice1: "question-4 choice-1",
+    choice2: "question-4 choice-2",
+    choice3: "question-4 choice-3",
+    choice4: "question-4 choice-4",
+    id: 'xxx',
+    answer: 4
+  },
+  { 
+    question: "question-5 question-5 question-5",
+    choice1: "question-5 choice-1",
+    choice2: "question-5 choice-2",
+    choice3: "question-5 choice-3",
+    choice4: "question-5 choice-4",
+    id: 'xxx',
+    answer: 2
+  },
+  { 
+    question: "question-6 question-6 question-6",
+    choice1: "question-6 choice-1",
+    choice2: "question-6 choice-2",
+    choice3: "question-6 choice-3",
+    choice4: "question-6 choice-4",
+    id: 'xxx',
+    answer: 1
+  },
+  { 
+    question: "question-7 question-7 question-7",
+    choice1: "question-7 choice-1",
+    choice2: "question-7 choice-2",
+    choice3: "question-7 choice-3",
+    choice4: "question-7 choice-4",
+    id: 'xxx',
+    answer: 3
+  },
+  { 
+    question: "question-8 question-8 question-8",
+    choice1: "question-8 choice-1",
+    choice2: "question-8 choice-2",
+    choice3: "question-8 choice-3",
+    choice4: "question-8 choice-4",
+    id: 'xxx',
+    answer: 2
+  },
+  { 
+    question: "question-9 question-9 question-9",
+    choice1: "question-9 choice-1",
+    choice2: "question-9 choice-2",
+    choice3: "question-9 choice-3",
+    choice4: "question-9 choice-4",
+    id: 'xxx',
+    answer: 4
+  },
+  { 
+    question: "question-10 question-10 question-10",
+    choice1: "question-10 choice-1",
+    choice2: "question-10 choice-2",
+    choice3: "question-10 choice-3",
+    choice4: "question-10 choice-4",
+    id: 'xxx',
+    answer: 2
+  }
+]
